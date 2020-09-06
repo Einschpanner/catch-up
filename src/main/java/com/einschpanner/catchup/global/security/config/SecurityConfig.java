3956b2c3fb2520fe0398255e4536e35a56ef19f6
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-09-06 13:48:40 +0900

    [CAT-12] feat: security config 추가 ## SecurityConfig  - Custom successHandler 추가  - JwtAuthenticationFilter 추가

--- src/main/java/com/einschpanner/catchup/global/security/config/SecurityConfig.java
+++ src/main/java/com/einschpanner/catchup/global/security/config/SecurityConfig.java
@@ -1,16 +1,22 @@
 package com.einschpanner.catchup.global.security.config;
 
-import com.einschpanner.catchup.global.security.dao.CustomOAuth2UserService;
+import com.einschpanner.catchup.global.security.filter.JwtAuthenticationFilter;
+import com.einschpanner.catchup.global.security.handler.OAuth2AuthenticationSuccessHandler;
+import com.einschpanner.catchup.global.security.provider.JwtTokenProvider;
+import com.einschpanner.catchup.global.security.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
-
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final JwtTokenProvider jwtTokenProvider;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -26,6 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .oauth2Login()
                         .userInfoEndpoint()
-                            .userService(customOAuth2UserService);
+                            .userService(customOAuth2UserService)
+                            .and()
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                .and()
+                    .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+        ;
     }
 }
