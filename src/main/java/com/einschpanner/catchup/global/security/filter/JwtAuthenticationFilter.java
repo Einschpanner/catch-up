b4dc42715bd6f5752e45c709ae4ad901d4796118
KimJinYounga <rlawlsdud419@skuniv.ac.kr>
2020-09-14 18:48:37 +0900

    [CAT-12] refactor: session.stateless로 로직 변경

--- src/main/java/com/einschpanner/catchup/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/einschpanner/catchup/global/security/filter/JwtAuthenticationFilter.java
@@ -13,40 +13,57 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
     ObjectMapper objectMapper;
 
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+//        Claims claims = null;
+//        try {
+//            claims = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+//            if (claims != null)
+//                SecurityContextHolder.getContext().setAuthentication(jwtTokenProvider.getAuthentication(claims));
+//            filterChain.doFilter(request, response);
+//        } catch (SignatureException e) {
+//            sendErrorMessage((HttpServletResponse) response, "유효하지 않은 토큰입니다.");
+//        } catch (MalformedJwtException e) {
+//            sendErrorMessage((HttpServletResponse) response, "손상된 토큰입니다.");
+//        } catch (DecodingException e) {
+//            sendErrorMessage((HttpServletResponse) response, "잘못된 인증입니다.");
+//        } catch (ExpiredJwtException e) {
+//            sendErrorMessage((HttpServletResponse) response, "만료된 토큰입니다.");
+//        }
+//    }
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         Claims claims = null;
         try {
-            claims = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+            claims = jwtTokenProvider.resolveToken(request);
             if (claims != null)
                 SecurityContextHolder.getContext().setAuthentication(jwtTokenProvider.getAuthentication(claims));
             filterChain.doFilter(request, response);
         } catch (SignatureException e) {
-            sendErrorMessage((HttpServletResponse) response, "유효하지 않은 토큰입니다.");
+            sendErrorMessage(response, "유효하지 않은 토큰입니다.");
         } catch (MalformedJwtException e) {
-            sendErrorMessage((HttpServletResponse) response, "손상된 토큰입니다.");
+            sendErrorMessage(response, "손상된 토큰입니다.");
         } catch (DecodingException e) {
-            sendErrorMessage((HttpServletResponse) response, "잘못된 인증입니다.");
+            sendErrorMessage(response, "잘못된 인증입니다.");
         } catch (ExpiredJwtException e) {
-            sendErrorMessage((HttpServletResponse) response, "만료된 토큰입니다.");
+            sendErrorMessage(response, "만료된 토큰입니다.");
         }
     }
 
