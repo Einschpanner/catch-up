767b874a2bd6b4403d520667b1bae82d83507304
KimJinYounga <rlawlsdud419@skuniv.ac.kr>
2020-09-18 12:41:02 +0900

    [CAT-26] unit test responseBody 빈값 반환 이슈 해결

--- src/test/java/com/einschpanner/catchup/global/common/RestDocsConfiguration.java
+++ src/test/java/com/einschpanner/catchup/global/common/RestDocsConfiguration.java
@@ -1,5 +1,7 @@
 package com.einschpanner.catchup.global.common;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -22,5 +24,18 @@ public class RestDocsConfiguration {
             }
         };
 
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        /* 연결 전략 : 같은 타입 + 필드명이 같은 경우만 동작 */
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
     }
 }
