a69223a7c922c46e32b9c04a2d40f133c6a12e94
KimJinYounga <rlawlsdud419@skuniv.ac.kr>
2020-10-10 21:27:42 +0900

    [CAT-16] feat: UserController에 profile 관련 기능 추가 # 원래 기획했던 API  - 프로필 불러오기 : /profiles/{user_id}  - 프로필 저장 : /profiles  - 프로필 수정 : /profiles  - 타임라인 불러오기(블로그리스트) : /profiles/blogs/{user_id}
    # 개발 진행하면서 API 변경
     - 프로필 저장 & 수정 : /profiles
     - 프로필 불러오기 & 타임라인 불러오기 :
     /profiles/{userId}
     ==> 프로필 불러올 때 블로그 글도 전부 가져옴

---
+++ src/main/java/com/einschpanner/catchup/domain/user/dto/ProfileDto.java
@@ -0,0 +1,77 @@
+package com.einschpanner.catchup.domain.user.dto;
+
+import com.einschpanner.catchup.domain.blog.domain.Blog;
+import com.einschpanner.catchup.domain.user.domain.User;
+import lombok.*;
+
+import java.util.List;
+
+public class ProfileDto {
+
+    @Setter
+    @Getter
+    @ToString
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class UpdateReq {
+        private String nickname;
+        private String urlProfile;
+        private String description;
+        private String addrRss;
+        private String addrGithub;
+        private String addrBlog;
+    }
+
+    @Getter
+    public static class UpdateRes {
+        private String nickname;
+        private String urlProfile;
+        private String description;
+        private String addrRss;
+        private String addrGithub;
+        private String addrBlog;
+        private String email;
+        private int cntFollowing;
+        private int cntFollower;
+
+        public UpdateRes(User user) {
+            this.nickname = user.getNickname();
+            this.urlProfile = user.getUrlProfile();
+            this.description = user.getDescription();
+            this.addrRss = user.getAddrRss();
+            this.addrGithub = user.getAddrGithub();
+            this.addrBlog = user.getAddrBlog();
+            this.email = user.getEmail();
+            this.cntFollowing = user.getCntFollowing();
+            this.cntFollower = user.getCntFollower();
+        }
+    }
+
+    @Getter
+    public static class Res {
+        private String nickname;
+        private String urlProfile;
+        private String description;
+        private String addrRss;
+        private String addrGithub;
+        private String addrBlog;
+        private String email;
+        private int cntFollowing;
+        private int cntFollower;
+        private List<Blog> blogs;
+
+
+        public Res(User user) {
+            this.nickname = user.getNickname();
+            this.urlProfile = user.getUrlProfile();
+            this.description = user.getDescription();
+            this.addrRss = user.getAddrRss();
+            this.addrGithub = user.getAddrGithub();
+            this.addrBlog = user.getAddrBlog();
+            this.email = user.getEmail();
+            this.cntFollowing = user.getCntFollowing();
+            this.cntFollower = user.getCntFollower();
+            this.blogs = user.getBlogs();
+        }
+    }
+}
