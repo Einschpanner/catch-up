fef0350ca96fcc8fdf2fc6be9349d7447db7a1cb
KimJinYounga <rlawlsdud419@skuniv.ac.kr>
2020-10-26 14:48:36 +0900

    [CAT-41] refactor : 모듈화 네이밍 수정

--- catchup-security/src/main/java/com/einschpanner/catchup/global/security/handler/OAuth2AuthenticationSuccessHandler.java
+++ catchup-security/src/main/java/com/einschpanner/catchup/global/security/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.einschpanner.catchup.global.security.handler;
 
+//import com.einschpanner.catchup.global.security.exception.UserNotFoundException;
 import com.einschpanner.catchup.global.security.provider.JwtTokenProvider;
 import com.einschpanner.catchup.domain.user.dao.UserRepository;
 import com.einschpanner.catchup.domain.user.domain.User;
@@ -9,8 +10,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import com.einschpanner.catchup.user.exception.UserNotFoundException;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -30,7 +31,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         DefaultOAuth2User oauth2User = (DefaultOAuth2User) authentication.getPrincipal();
         String email = (String) oauth2User.getAttributes().get("email");
-        User user = userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
+        User user = userRepository.findByEmail(email).orElseThrow(EntityNotFoundException::new);
+//        User user = userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
         String token = jwtTokenProvider.createToken(user.getUserId(), Collections.singletonList("ROLE_USER"));
 
         response.addHeader("X-Auth-Token", "Bearer  " + token);
